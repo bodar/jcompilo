49e9565ca1d84c9bae314728e6da4d205ff7fb0e
Daniel Worthington-Bodart <dan@bodar.com>
2013-02-23 21:43:07 +0000

    Added support for multiple lambdas

--- example/src/com/example/UsesLambda.java
+++ example/src/com/example/UsesLambda.java
@@ -9,7 +9,14 @@ import static com.googlecode.totallylazy.lambda.Lambdas.λ;
 
 public class UsesLambda {
     @lambda
-    public Sequence<Character> lambdaExample(){
+    public Sequence<Character> singleLambda(){
         return sequence("dan", "matt").map(λ(s, s.charAt(0)));
+    }
+
+    @lambda
+    public Sequence<Character> multipleLambdas(){
+        return sequence("dan", "matt").
+                map(λ(s, s.toUpperCase())).
+                map(λ(s, s.charAt(0)));
     }
 }
--- example/test/com/example/UsesLambdaTest.java
+++ example/test/com/example/UsesLambdaTest.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import com.googlecode.totallylazy.Sequence;
 import org.junit.Test;
 
 import static com.googlecode.totallylazy.Sequences.sequence;
@@ -9,8 +8,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UsesLambdaTest {
     @Test
-    public void canProcessLambda() throws Exception {
-        Sequence<Character> characters = new UsesLambda().lambdaExample();
-        assertThat(characters, is(sequence('d', 'm')));
+    public void handlesSingleLambda() throws Exception {
+        assertThat(new UsesLambda().singleLambda(), is(sequence('d', 'm')));
+    }
+
+    @Test
+    public void handlesMultipleLambdas() throws Exception {
+        assertThat(new UsesLambda().multipleLambdas(), is(sequence('D', 'M')));
     }
 }
