fc45cf4d4c089169d4c7e05eab70234606c324ba
Daniel Worthington-Bodart <dan@bodar.com>
2014-02-11 13:45:20 +0000

    Added Windows script (requires wget)

---
+++ example/jcompilo.cmd
@@ -0,0 +1,27 @@
+@echo off
+
+setlocal
+set JAVA_OPTS="-Djava.net.useSystemProxies=true %JAVA_OPTS%"
+set BUILD_NUMBER=%BUILD_NUMBER%
+set version=@version@
+set artifact=jcompilo
+set group=com/googlecode/%artifact%
+set repo=repo.bodar.com
+set dir=lib
+set jar=%dir%\%artifact%.jar
+set pack=%dir%\%artifact%.pack.gz
+set url=http://%repo%/%group%/%artifact%/%version%/%artifact%-%version%
+set remote_file=%url%.pack.gz
+set remote_sh=%url%.cmd
+
+if /i "%1"=="update" (
+    del /q %jar% %pack% 2>null
+)
+
+if not exist %jar% ( 
+    mkdir %dir% 
+    wget -O %pack% %remote_file%
+    unpack200 %pack% %jar% 
+    del /q %pack%
+)
+java -showversion -Dbuild.number=%BUILD_NUMBER% %JAVA_OPTS% -jar %jar% %*
