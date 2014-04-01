c2493bdb0b870306eadb96f1ac7c390ce9a1374f
Daniel Worthington-Bodart <dan@bodar.com>
2014-04-01 09:56:06 +0100

    Minor clean up

--- src/com/googlecode/jcompilo/tests/junit/AntTestOutput.java
+++ src/com/googlecode/jcompilo/tests/junit/AntTestOutput.java
@@ -148,7 +148,7 @@ public class AntTestOutput extends RunListener {
 
     private String handleFailure(Map<Field, Object> fields) {
         if (!fields.containsKey(Field.Type)) return "";
-        return applyTemplate(fields.get(Field.Type).toString().toLowerCase(), new Object[]{Xml.escape(fields.get(Field.Message)), Xml.escape(fields.get(Field.ExceptionType)), Xml.escape(fields.get(Field.FullMessage))});
+        return applyTemplate(fields.get(Field.Type).toString().toLowerCase(), Xml.escape(fields.get(Field.Message)), Xml.escape(fields.get(Field.ExceptionType)), Xml.escape(fields.get(Field.FullMessage)));
     }
 
     private Object calculateTime(Map<Field, Object> fields) {
