34aacc1164ed8428f3ec906514fb821fd5cdab93
Daniel Worthington-Bodart <dan@bodar.com>
2014-09-08 12:14:20 +0100

    More clean up

--- src/com/googlecode/jcompilo/bytecode/ConstantPoolMapper.java
+++ src/com/googlecode/jcompilo/bytecode/ConstantPoolMapper.java
@@ -14,13 +14,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
 
 import static com.googlecode.totallylazy.Closeables.using;
-import static com.googlecode.totallylazy.Sequences.sequence;
 
 public class ConstantPoolMapper {
     private final Unary<String> mapper;
@@ -35,7 +30,8 @@ public class ConstantPoolMapper {
             public ConstantPoolMapper call(Sources sources, final Destination destination) throws Exception {
                 for (Sources.Source source : sources.sources()) {
                     OutputStream outputStream = destination.destination(mapper.call(source.name), source.modified);
-                    if (source.name.endsWith(".class")) using(source.input, outputStream, functions.process(ConstantPoolMapper.this));
+                    if (source.name.endsWith(".class"))
+                        using(source.input, outputStream, functions.process(ConstantPoolMapper.this));
                     else Streams.copyAndClose(source.input, outputStream);
                 }
                 return ConstantPoolMapper.this;
@@ -65,35 +61,18 @@ public class ConstantPoolMapper {
             int tag = in.readUnsignedByte();
             out.writeByte(tag);
 
-            switch (tag) {
-                case CONSTANT_UTF8:
+            Constant constant = Constant.constant(tag);
+            switch (constant) {
+                case Utf8:
                     out.writeUTF(Callers.call(mapper, in.readUTF()));
                     break;
-                case CONSTANT_CLASS:
-                case CONSTANT_STRING:
-                case CONSTANT_MethodType:
-                    copy(in, out, 2);
-                    break;
-                case CONSTANT_INTEGER:
-                case CONSTANT_FLOAT:
-                case CONSTANT_FIELD:
-                case CONSTANT_METHOD:
-                case CONSTANT_INTERFACEMETHOD:
-                case CONSTANT_NAMEANDTYPE:
-                case CONSTANT_InvokeDynamic:
-                    copy(in, out, 4);
-                    break;
-                case CONSTANT_LONG:
-                case CONSTANT_DOUBLE:
-                    copy(in, out, 8);
+                case Double:
+                case Long:
                     i++; // "In retrospect, making 8-byte constants take two constant pool entries was a poor choice."
                     // See http://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.4.5
-                    break;
-                case CONSTANT_MethodHandle:
-                    copy(in, out, 3);
-                    break;
                 default:
-                    throw new ClassFormatError("Unknown tag: " + tag + " in " + in);
+                    copy(in, out, constant.size);
+                    break;
             }
         }
         Streams.copyAndClose(in, out);
@@ -106,21 +85,6 @@ public class ConstantPoolMapper {
         in.readFully(buffer, 0, amount);
         out.write(buffer, 0, amount);
     }
-
-    public static final int CONSTANT_UTF8 = 1;
-    public static final int CONSTANT_INTEGER = 3;
-    public static final int CONSTANT_FLOAT = 4;
-    public static final int CONSTANT_LONG = 5;
-    public static final int CONSTANT_DOUBLE = 6;
-    public static final int CONSTANT_CLASS = 7;
-    public static final int CONSTANT_STRING = 8;
-    public static final int CONSTANT_FIELD = 9;
-    public static final int CONSTANT_METHOD = 10;
-    public static final int CONSTANT_INTERFACEMETHOD = 11;
-    public static final int CONSTANT_NAMEANDTYPE = 12;
-    public static final int CONSTANT_MethodHandle = 15;
-    public static final int CONSTANT_MethodType = 16;
-    public static final int CONSTANT_InvokeDynamic = 18;
 
     public static class functions {
         public static Function2<InputStream, OutputStream, ConstantPoolMapper> process(final ConstantPoolMapper poolMapper) {
